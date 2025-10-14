76d183270382b4db0d02f0fe4ac606f57e1b6c46
Alejandra Marín Rincón <alejandra.marin@sofka.local>
2025-10-14 16:11:13 -0500

    se habilito cors en el bakend

--- Backend/proyecto/settings.py
+++ Backend/proyecto/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -51,6 +53,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "proyecto.urls"
+
+#Permite todas las conexiones (solo para desarrollo)
+#en produccion (CORS_ALLOWED_ORIGINS).
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 TEMPLATES = [
     {
