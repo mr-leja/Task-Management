7cbd0e154e61c58567095eb8f0e81b69f26f5dd5
Alejandra Marín Rincón <alejandra.marin@sofka.local>
2025-10-15 13:43:39 -0500

    se borro rutas de la app autenticacion sin uso

--- Backend/proyecto/urls.py
+++ Backend/proyecto/urls.py
@@ -6,5 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("app.urls")),
-    path("api/", include("autenticacion.urls")), 
 ]
