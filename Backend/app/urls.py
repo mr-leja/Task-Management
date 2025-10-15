9efdee7fd6f2bfd9b833c6c874772da7cfd117a5
Alejandra Marín Rincón <alejandra.marin@sofka.local>
2025-10-14 20:03:02 -0500

    se configuro la ruta api para unir al frontend

--- Backend/app/urls.py
+++ Backend/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listar_tareas, name='listar_tareas'),
+    path('tareas/', views.listar_tareas, name='listar_tareas'),  # 👈 agregado "/"
     path('tareas/crear/', views.crear_tarea, name='crear_tarea'),
     path('tareas/<int:pk>/', views.obtener_tarea, name='obtener_tarea'),
     path('tareas/<int:pk>/actualizar/', views.actualizar_tarea, name='actualizar_tarea'),
